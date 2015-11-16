8195c1859f99a30298763bd479bcb3ab3ae48c19
ruszkowskij <ruszkowskij@wp.pl>
2015-11-16 21:53:42 +0100

    Pochodna - makro

--- polynomial.lisp
+++ polynomial.lisp
@@ -43,3 +43,10 @@
 	    (remove nil (cons (list (* (GetFactor (first poli)) (GetPower (first poli))) (- (GetPower (first poli)) 1)) (PolyDerivative (rest poli))))
     )
 )
+
+(defun PolyDerivativeM(poli)
+"Zwraca pochodną wielomianu"
+	(if (and (not (eq nil poli)) (> (GetPower (first poli)) 0))
+	    (remove nil (cons (list `(* ,(GetFactor (first poli)) ,(GetPower (first poli))) `(- ,(GetPower (first poli)) 1)) (PolyDerivativeM (rest poli))))
+    )
+)
