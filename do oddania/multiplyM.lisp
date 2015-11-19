1522eccb5d3bc9dffc0cd3cee99dbe0e1b971b03
ruszkowskij <ruszkowskij@wp.pl>
2015-11-19 02:54:58 +0100

    multiply, power makra - testy

--- do oddania/multiplyM.lisp
+++ do oddania/multiplyM.lisp
@@ -6,3 +6,10 @@
 		    ``(* ,,x ,(multiply-macro ,@y)))
 	)
 )
+
+(print (multiply-macro 1 2 3 4))
+(print (eval(multiply-macro 1 2 3 4)))
+(print (multiply-macro `(* 5 2)  1))
+(print (eval(multiply-macro `(* 5 2)  1)))
+(print (multiply-macro `(+ (* 5 2) 3) `(+ 3 4)))
+(print (eval(multiply-macro `(+ (* 5 2) 3) `(+ 3 4))))
--- do oddania/powerM.lisp
+++ do oddania/powerM.lisp
@@ -8,3 +8,14 @@
             `(* (/ 1 ,,x) ,(power-macro ,x (+ ,y 1))))
     )
 )
+
+(print (power-macro 3 2))
+(print (eval(power-macro 3 2)))
+
+(setq x (power-macro '(+ a b) 3))
+
+(setf a 1)
+(setf b 3)
+
+(print x)
+(print (eval x))
