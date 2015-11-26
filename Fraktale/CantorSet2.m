47b91d89930c800cf87aac1c69803f3ec8a7c43f
ruszkowskij <ruszkowskij@wp.pl>
2015-11-26 19:33:36 +0100

    Uogółniony zbiór Cantora

---
+++ Fraktale/CantorSet2.m
@@ -0,0 +1,23 @@
+function [ ] = CantorSet( iter, part )
+
+    x = [0, 1];
+    for i = 1:iter
+        b_old = x;
+        b = reshape(b_old, 2, []);
+        l = ones(1, 2) * i;
+        for k = 1:size(b, 2)
+            plot(b(:, k), l, 'ro-'), hold on
+        end
+        x_new = [];
+        for j = 1:size(b, 2)
+                x1 = b(1,j);
+                x2 = b(2,j);
+                d = ((x2 - x1) - (x2 - x1) ./ part) / 2;
+                x_new = [x_new, x1, x1 + d, x2 - d, x2];
+        end
+        x = x_new;
+    end
+    xlim([-0.1 1.1]), ylim([0 iter+1])
+
+end
+    
