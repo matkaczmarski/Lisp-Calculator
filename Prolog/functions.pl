00e47c55ed30330011c06de813c1e673d1c1ce69
mateusz <matkaczmarski@interia.pl>
2015-12-02 21:51:09 +0100

    Prolog: exp

--- Prolog/functions.pl
+++ Prolog/functions.pl
@@ -1,6 +1,6 @@
 setMaximumIterations(Iterations):-
     Iterations is 20.
-
+% --------------------------------------------------------
 %FACTORIAL/SILNIA: x! = Result
 factorial(0,Result) :-
     Result is 1.
@@ -10,11 +10,24 @@ factorial(X,Result) :-
     factorial(X1,Result1),
     Result is Result1*X.
 
+% --------------------------------------------------------
 %POWER/POTÊGOWANIE: x^y = Result
 power(X,0,Result):-
-Result is 1.
+    Result is 1.
 power(X,Y,Result):-
-        Y > 0,
-        Y1 is Y - 1,
-        power(X,Y1,Z),
-        Result is Z*X.
+    Y > 0,
+    Y1 is Y - 1,
+    power(X,Y1,Z),
+    Result is Z*X.
+
+% --------------------------------------------------------
+%EXPONENTIAL: e^x = Result in Iter iterations
+exp(X,Result,0):-
+        Result is 1.
+exp(X,Result,Iter):-
+    Iter > 0,
+    Iter1 is Iter - 1,
+    exp(X,Result1,Iter1),
+    factorial(Iter,Factorial),
+    power(X,Iter,Power),
+    Result is Result1 + Power / Factorial.
