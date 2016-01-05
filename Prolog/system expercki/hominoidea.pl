5c77af7673df51171772c6802f6c06c508c8359d
ruszkowskij <ruszkowskij@wp.pl>
2016-01-05 23:26:11 +0100

    średnia waga / wzrost

--- Prolog/system expercki/hominoidea.pl
+++ Prolog/system expercki/hominoidea.pl
@@ -8,6 +8,35 @@ hominoidea('Phoebe', 'Hylobatidae', '', '', 'Hylobates', 'Hylobates moloch', '6.
 hominoidea('Abby', 'Hominidae', 'Ponginae', '', 'Pongo', 'Pongo abelii', '49.0', '94.0', '32', 'Female', 'Omnivore').
 hominoidea('Rob', 'Hominidae', 'Homininae', 'Gorillini', 'Gorilla', 'Gorilla gorilla', '135.0', '174.0', '28', 'Male', 'Omnivore').
 
+get_average_height_by_species(Average) :-
+	write('Enter name of a specie: '),
+	read_line_to_codes(user_input, SpecieInput),
+	atom_codes(Specie, SpecieInput), nl,
+	findall(Height, hominoidea(_,_,_,_,_,Specie,_,Height,_,_,_), Animals),
+	average(Average,Animals).
+
+get_average_weight_by_species(Average) :-
+	write('Enter name of a specie: '),
+	read_line_to_codes(user_input, SpecieInput),
+	atom_codes(Specie, SpecieInput), nl,
+	findall(Height, hominoidea(_,_,_,_,_,Specie,_,Height,_,_,_), Animals),
+	average(Average,Animals).
+
+average(Average, List) :- sum(List, Sum),
+                          my_length(List, Count),
+                          Average is Sum/Count.
+
+sum([], 0).
+sum([X|List], Sum) :-
+    sum(List, Sum1),
+    atom_number(X, Xnumber),
+    Sum is Xnumber + Sum1.
+
+my_length([], Count) :-
+	Count is 1.
+my_length(List, Count) :-
+	length(List, Count).
+
 get_by_species() :-
     write('Enter name of a specie: '),
     read_line_to_codes(user_input, SpecieInput),
@@ -18,7 +47,7 @@ get_by_species() :-
     writef('Found %w individuals:\n', [AnimalsLen]),
     format('~w~t~10|~w~t~25|~w~t~40|~w~t~55|~w~t~65|~w~t~85|~w~t~100|~w~t~115|~w~t~130|~w~t~145|~w~t~160|~n', ['Name', 'Superfamily', 'Subfamily', 'Tribe', 'Genus', 'Specie', 'Weight (kg)', 'Height (cm)', 'Age', 'Sex', 'Diet']),
     foreach(member(IndividualInfo, UniqueAnimals), format('~w~t~10|~w~t~25|~w~t~40|~w~t~55|~w~t~65|~w~t~85|~w~t~100|~w~t~115|~w~t~130|~w~t~145|~w~t~160|~n', IndividualInfo)).%  writef('\t%10L%w %10L%w %10L%w %10L%w %10L%w %10L%w %10L%w %10L%w %10L%w %10L%w %10L%w\n', IndividualInfo)).
-    
+
 add_individual() :-
     write('Define new individual:'), nl, nl,
     write('Name: '),
@@ -56,11 +85,15 @@ add_individual() :-
     atom_codes(Diet, DietInput),
     assertz(hominoidea(Name, Family, Subfamily, Tribe, Genus, Specie, Weight, Height, Age, Sex, Diet)),
     write('Individual added.').
-    
+
 remove_individual() :-
     write("Enter name of individual to remove:"),
     read_line_to_codes(user_input, NameInput),
     atom_codes(Name, NameInput),
     retractall(hominoidea(Name, _, _, _, _, _, _, _, _, _, _)),
     write('Individual removed.').
+
+
+
+
 
