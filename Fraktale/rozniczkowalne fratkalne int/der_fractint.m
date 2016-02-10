07b5a7c13f35b12bb8026f14a709705638d4f3d0
mateusz <matkaczmarski@interia.pl>
2016-02-11 00:25:06 +0100

    liczenie pochodnych dla obu serii

--- Fraktale/rozniczkowalne fratkalne int/der_fractint.m
+++ Fraktale/rozniczkowalne fratkalne int/der_fractint.m
@@ -1,15 +1,14 @@
 function [ params] = der_fractint(x, y, s, k)
 
 %ile pktów braæ do interpolacji hermite'a
-pointsPerHI = 4;
+pointsPerHI = 2;
 
-% tu coœ zmieniæ, bo jest œrednio ok
+
 [xi,yi] = safif([x' y'], s, length(s), k)
 [sortedX, sortIndex] = sort(xi);
 sortedY = yi(sortIndex);
 xi=sortedX;
 yi=sortedY;
-% koniec zmian
 
 n = length(xi)-1;
 pphi = pointsPerHI -1;
@@ -18,6 +17,12 @@ rest = mod(n, pphi);
 
 params = [];
 
+prev_der_val = 0;
+next_der_val = 0;
+x_prev = 0;
+y_prev = 0;
+x_next = 0;
+y_next = 0;
 for i=1:seg
     ind = (i-1)*pphi;
     p=[];
@@ -25,40 +30,91 @@ for i=1:seg
     ytmp = [];
     
     for j=1:pphi+1
+        if j == pphi
+            x_prev = xi(ind+j);
+            y_prev = yi(ind+j);
+        end
         xtmp=[xtmp,xi(ind+j)];
         ytmp=[ytmp,yi(ind+j)];
     end
 
     if i == 1
+        x_next = xi(pointsPerHI+1);
+        y_next = yi(pointsPerHI+1);
+        
+        %wyliczamy wartoœæ pochodnej dla punktu œrodkowego miêdzy seriami
+        common_x = [x_prev, xtmp(end), x_next];
+        common_y = [y_prev,ytmp(end),y_next];
         dx=[];
         dy=[];
+        [a1 b1 c1 d1] = hermite_cubic_interpolate(common_x,common_y,dx,dy);     
+        
+        dx=[xtmp(end)]
+        next_der_val = polyval(polyder([a1 b1 c1 d1]), xtmp(end));
+        dy=[next_der_val]
         [a b c d] = hermite_cubic_interpolate(xtmp,ytmp,dx,dy);
         params = [params; xtmp(1), xtmp(pointsPerHI),a,b,c,d];
+    elseif i == seg
+               
+        if rest ~= 0
+            %ostatni segment przed rest:
+            prev_der_val = next_der_val;
+            x_next = xi(ind + pointsPerHI + 1);
+            y_next = yi(ind + pointsPerHI + 1);
+            
+            %wyliczamy wartoœæ pochodnej dla punktu œrodkowego miêdzy seriami
+            common_x = [x_prev, xtmp(end), x_next];
+            common_y = [y_prev,ytmp(end),y_next];
+            dx=[];
+            dy=[];
+            [a1 b1 c1 d1] = hermite_cubic_interpolate(common_x,common_y,dx,dy);
+            
+            dx=[xtmp(1),xtmp(end)];
+            next_der_val = polyval(polyder([a1 b1 c1 d1]), xtmp(end));
+            dy=[prev_der_val, next_der_val];
+            [a b c d] = hermite_cubic_interpolate(xtmp,ytmp,dx,dy);
+            params = [params; xtmp(1), xtmp(pointsPerHI),a,b,c,d];
+            
+            %dla rest
+            xtmp = [];
+            ytmp=[];
+            for i=rest:-1:0
+                xtmp = [xtmp, xi(length(xi)-i)];
+                ytmp = [ytmp, yi(length(yi)-i)];
+            end
+            dx=[xtmp(1)];
+            dy=[next_der_val];
+            [a b c d] = hermite_cubic_interpolate(xtmp,ytmp,dx,dy);
+            params = [params; xtmp(1), xtmp(end),a,b,c,d];
+        else
+            prev_der_val = next_der_val;
+            dx=[xtmp(1)];
+            dy=[prev_der_val];
+            [a b c d] = hermite_cubic_interpolate(xtmp,ytmp,dx,dy);
+            params = [params; xtmp(1), xtmp(pointsPerHI),a,b,c,d];
+        end
     else
-        dx=[xtmp(1)];
-        prev_params=params(size(params,1),:);
-        der_x1=polyder(prev_params(pphi:end));
-        dy=[polyval(der_x1, xtmp(1))];
+        prev_der_val = next_der_val;
+        x_next = xi(ind + pointsPerHI + 1);
+        y_next = yi(ind + pointsPerHI + 1);
+        
+        %wyliczamy wartoœæ pochodnej dla punktu œrodkowego miêdzy seriami
+        common_x = [x_prev, xtmp(end), x_next];
+        common_y = [y_prev,ytmp(end),y_next];
+        dx=[];
+        dy=[];
+        [a1 b1 c1 d1] = hermite_cubic_interpolate(common_x,common_y,dx,dy);
+               
+        dx=[xtmp(1),xtmp(end)];
+        next_der_val = polyval(polyder([a1 b1 c1 d1]), xtmp(end));
+        dy=[prev_der_val, next_der_val];
         [a b c d] = hermite_cubic_interpolate(xtmp,ytmp,dx,dy);
         params = [params; xtmp(1), xtmp(pointsPerHI),a,b,c,d];
     end    
 end
 
 
-if rest ~= 0
-    xtmp = [];
-    ytmp=[];
-    for i=rest:-1:0
-        xtmp = [xtmp, xi(length(xi)-i)];
-        ytmp = [ytmp, yi(length(yi)-i)];
-    end
-    dx=[xtmp(1)]
-    prev_params=params(size(params,1),:)
-    der_x1=polyder(prev_params(3:end));
-    dy=[polyval(der_x1, xtmp(1))];
-    [a b c d] = hermite_cubic_interpolate(xtmp,ytmp,dx,dy);
-    params = [params; xtmp(1), xtmp(end),a,b,c,d];
-end
+
 
 %xi
 %yi
